7a9595e68e3e89e25e1c01142f88e12ebfd842a8
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-06-07 15:12:40 +0200

    qq

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,15 +2,44 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 const forma = document.querySelector('.form');
-const inputDelay = document.querySelector('input[name="delay"]');
-const inputType = document.querySelector('input[name="state"]');
+
 
 
 
 forma.addEventListener('submit', (event) => {
     event.preventDefault();
-
+    const inputType = document.querySelector('input[name="state"]:checked');
+    const delay = parseInt(document.querySelector('input[name="delay"]').value, 10);
+    createPromise(inputType.value, delay)
+        .then( (result) => {
+        iziToast.show({
+    message: result,
+    backgroundColor: '#4caf50',
+    messageColor: '#ffffff',
+    position: 'topRight',
+    timeout: 3000,
+    transitionIn: 'fadeIn',
+    transitionOut: 'fadeOut',
+    icon: 'icon-person',
+    iconText: '✔',
+    iconColor: '#fff',
+});
+        })
+        .catch((error) => {
+             iziToast.show({
+    message: error,
+    backgroundColor: '#e57373',
+    messageColor: '#ffffff',
+    position: 'topRight',
+    timeout: 3000,
+    transitionIn: 'fadeIn',
+    transitionOut: 'fadeOut',
+    icon: 'icon-person',
+    iconText: '\u2716',
+    iconColor: 'red',
+        })
     });
+});
 
 
 
@@ -26,21 +55,8 @@ return new Promise( (resolve, reject) => {
 })
 
 }
-console.log(inputType);
 
 
 
 
 
-
-       iziToast.show({
-    message: `Fulfilled promise in ms`,
-    backgroundColor: '#4caf50',
-    position: 'topRight',
-    timeout: 3000,
-    transitionIn: 'fadeIn',
-    transitionOut: 'fadeOut',
-    icon: 'icon-person',
-    iconText: '✔',
-    iconColor: '#fff',
-});
